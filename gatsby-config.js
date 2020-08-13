bce501a8f99a96a37a51ce95f9b4cf2636d98267
Fabian Cano <fabian.cano@correounivalle.edu.co>
2020-08-13 17:36:11 -0500

    baseUrl was updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    baseUrl: `https://random-netlify-url.netlify.com`,
+    baseUrl: `https://fabianandrescano.com`,
   },
   plugins: [
     {
